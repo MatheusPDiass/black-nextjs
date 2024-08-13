b8b6c1f137ed7c3287e5d7ef8f88c55067fbe2df
MatheusPDiass <matiospd94@gmail.com>
2024-08-13 15:47:05 -0300

    last updates

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,6 +1,9 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import Header from "../src/components/Header";
+import CartTable from "../src/components/CartTable";
+import { Container } from "reactstrap";
+import CartTotal from "@/components/CartTotal";
 
 const Cart: NextPage = () => {
   return (
@@ -13,7 +16,14 @@ const Cart: NextPage = () => {
 
       <Header />
 
-      <h1>Carrinho</h1>
+      <main>
+        <Container className="mb-5">
+          <h1 className="my-5">Carrinho</h1>
+
+          <CartTable />
+          <CartTotal />
+        </Container>
+      </main>
     </>
   );
 };
